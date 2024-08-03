218ef615374c7b5fbfd9b5801756ec9f8751d274
qowevisa <qowevisa@gmail.com>
2024-08-03 10:57:13 +0300

    Fixes

--- db/category.go
+++ db/category.go
@@ -17,18 +17,22 @@ type Category struct {
 }
 
 var (
-	ERROR_CATEGORY_PARENT_NOT_FOUND = errors.New("ParentID is invalid for user")
-	ERROR_CATEGORY_NAME_NOT_UNIQUE  = errors.New("Name for Category have to be unique for user")
+	ERROR_CATEGORY_PARENT_NOT_FOUND  = errors.New("ParentID is invalid for user")
+	ERROR_CATEGORY_NAME_NOT_UNIQUE   = errors.New("Name for Category have to be unique for user")
+	ERROR_CATEGORY_USER_ID_NOT_EQUAL = errors.New("ParentID is invalid for user")
 )
 
 func (c *Category) BeforeSave(tx *gorm.DB) error {
 	if c.ParentID != 0 {
 		var parent Category
-		if err := tx.Find(&parent, Category{ParentID: c.ParentID, UserID: c.UserID}).Error; err != nil {
+		if err := tx.Find(&parent, c.ID).Error; err != nil {
 			return err
 		}
 		if parent.ID == 0 {
 			return ERROR_CATEGORY_PARENT_NOT_FOUND
+		}
+		if parent.UserID != c.UserID {
+			return ERROR_CATEGORY_USER_ID_NOT_EQUAL
 		}
 	}
 	var dup Category
