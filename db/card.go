40fa706514eb82a215b236c8dc48c29cb2f43108
qowevisa <qowevisa@gmail.com>
2024-08-03 10:44:06 +0300

    Add BeforeSave to card

--- db/card.go
+++ db/card.go
@@ -1,6 +1,10 @@
 package db
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 // Card can be either card or wallet
 type Card struct {
@@ -11,4 +15,26 @@ type Card struct {
 	CreditLine     uint64
 	UserID         uint
 	User           *User
+}
+
+var (
+	ERROR_CARD_NAME_EMPTY      = errors.New("Card's name can't be empty")
+	ERROR_CARD_NAME_NOT_UNIQUE = errors.New("Card's name have to be unique for user")
+)
+
+func (c *Card) BeforeSave(tx *gorm.DB) error {
+	if c.Name == "" {
+		return ERROR_CARD_NAME_EMPTY
+	}
+
+	var dup Card
+	if err := tx.Find(&dup, Card{Name: c.Name}).Error; err != nil {
+		return err
+	}
+
+	if dup.ID != 0 {
+		return ERROR_CARD_NAME_NOT_UNIQUE
+	}
+
+	return nil
 }
