0947a4fdf51a93223c63635186a019f9a5af1923
qowevisa <qowevisa@gmail.com>
2024-08-12 18:31:07 +0300

    Add DbDebt to types

--- types/types.go
+++ types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "time"
+
 // User struct for requests
 type User struct {
 	Username string `json:"username" binding:"required" example:"testUser"`
@@ -35,4 +37,16 @@ type DbCategory struct {
 	//  Can be 0
 	ParentID uint `json:"parent_id" example:"0"`
 	UserID   uint `json:"user_id" example:"1"`
+}
+
+type DbDebt struct {
+	ID       uint      `json:"id" example:"1"`
+	CardID   uint      `json:"card_id" example:"1"`
+	Comment  string    `json:"comment" example:"pizza"`
+	Value    uint64    `json:"value" example:"20000"`
+	IOwe     bool      `json:"i_owe" example:"true"`
+	Date     time.Time `json:"date" example:"29/11/2001 12:00"`
+	DateEnd  time.Time `json:"date_end" example:"29/12/2001 12:00"`
+	Finished bool      `json:"finished" example:"false"`
+	UserID   uint      `json:"user_id" example:"1"`
 }
