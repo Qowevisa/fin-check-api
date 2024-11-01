e2d3d636cb4c337f2e5788a0c6ff1c6985a11ebe
qowevisa <qowevisa@gmail.com>
2024-11-01 09:30:34 +0200

    Add TotalCost MetricValue MetricType and Type relationship to ItemBought

--- db/item_bought.go
+++ db/item_bought.go
@@ -4,9 +4,14 @@ import "gorm.io/gorm"
 
 type ItemBought struct {
 	gorm.Model
-	ItemID    uint
-	Item      *Item
-	Quantity  uint
-	PaymentID uint
-	Payment   *Payment
+	ItemID      uint
+	Item        *Item
+	PaymentID   uint
+	Payment     *Payment
+	TypeID      uint
+	Type        *Type
+	Quantity    uint
+	TotalCost   uint64
+	MetricType  uint8
+	MetricValue uint64
 }
