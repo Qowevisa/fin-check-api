7f4067ce31e9edb04da9e43cd2c6aedc470960db
qowevisa <qowevisa@gmail.com>
2024-11-02 00:05:28 +0200

    Add implemetation for db.UserIdentifiable interface for Debt entity

--- db/debt.go
+++ db/debt.go
@@ -19,3 +19,18 @@ type Debt struct {
 	UserID   uint
 	User     *User
 }
+
+// Implements db.UserIdentifiable:1
+func (d Debt) GetID() uint {
+	return d.ID
+}
+
+// Implements db.UserIdentifiable:2
+func (d Debt) GetUserID() uint {
+	return d.UserID
+}
+
+// Implements db.UserIdentifiable:3
+func (d *Debt) SetUserID(id uint) {
+	d.UserID = id
+}
